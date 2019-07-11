9ab4a7b519ba5f919236e938546b747c86f6405b
Felix HH Yeung <felixhhyeung@hotmail.com>
2019-07-11 15:29:13 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 	REFRESHMILLI = 100;
 
-	hourlyPay = 50;
+	hourlyPay = 58.82;
 	money = 0;
 	timeStart = "0900";
 	timeSpan: number;
 	timeSpanString: string;
 	wSTTimestamp: number;
+	menuIsShown = true;
 	constructor() {}
 	ngOnInit() {
 		console.log("ngOnInit()");
@@ -22,15 +23,28 @@ export class AppComponent implements OnInit {
 			this.update();
 		}, this.REFRESHMILLI);
 	}
+
 	init() {
+		console.log("init()");
 		this.wSTTimestamp = new Date().setHours(parseInt(this.timeStart.substring(0, 2)), parseInt(this.timeStart.substring(2, 4)), 0, 0);
+		console.log("this.wSTTimestamp: " + this.wSTTimestamp);
 	}
+
 	update() {
 		const nowTimestamp = new Date();
 		this.timeSpan = (nowTimestamp - this.wSTTimestamp);
 		this.timeSpanString = this.msToTime(this.timeSpan);
-		this.money = 50 / 60 / 60 / 1000 * this.timeSpan;
+		this.money = this.hourlyPay / 60 / 60 / 1000 * this.timeSpan;
 	}
+
+	hideMenuClick() {
+		this.menuIsShown = false;
+	}
+
+	showMenuClick() {
+		this.menuIsShown = true;
+	}
+
 	msToTime(s) {
 	  var ms = s % 1000;
 	  s = (s - ms) / 1000;
@@ -38,7 +52,6 @@ export class AppComponent implements OnInit {
 	  s = (s - secs) / 60;
 	  var mins = s % 60;
 	  var hrs = (s - mins) / 60;
-
-	  return hrs + ':' + mins + ':' + secs + '.' + ms;
+	  return hrs.toString().padStart(2, "0") + ':' + mins.toString().padStart(2, "0") + ':' + secs.toString().padStart(2, "0") + '.' + ms.toString().padStart(3, "0");
 	}
 }
