d385788bcfbb5452d49e0a01213d934c5ae53fdb
Felix HH Yeung <felixhhyeung@hotmail.com>
2019-07-11 14:36:16 +0800

    basic functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// components
+import { MainComponent } from './pages/main/main.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{ path: 'main', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WorkTimer';
+export class AppComponent implements OnInit {
+	REFRESHMILLI = 100;
+
+	hourlyPay = 50;
+	money = 0;
+	timeStart = "0900";
+	timeSpan: number;
+	timeSpanString: string;
+	wSTTimestamp: number;
+	constructor() {}
+	ngOnInit() {
+		console.log("ngOnInit()");
+		this.init();
+		setInterval(() => {
+			this.update();
+		}, this.REFRESHMILLI);
+	}
+	init() {
+		this.wSTTimestamp = new Date().setHours(parseInt(this.timeStart.substring(0, 2)), parseInt(this.timeStart.substring(2, 4)), 0, 0);
+	}
+	update() {
+		const nowTimestamp = new Date();
+		this.timeSpan = (nowTimestamp - this.wSTTimestamp);
+		this.timeSpanString = this.msToTime(this.timeSpan);
+		this.money = 50 / 60 / 60 / 1000 * this.timeSpan;
+	}
+	msToTime(s) {
+	  var ms = s % 1000;
+	  s = (s - ms) / 1000;
+	  var secs = s % 60;
+	  s = (s - secs) / 60;
+	  var mins = s % 60;
+	  var hrs = (s - mins) / 60;
+
+	  return hrs + ':' + mins + ':' + secs + '.' + ms;
+	}
 }
